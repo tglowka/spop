9825ef1a74181801eded53bb36ca32fcc273a452
tglowka <38429856+tglowka@users.noreply.github.com>
2020-12-06 16:37:50 +0100

    Tglowka/add helper functions (#2)
    * Dodanie funkcji pomocniczych: getAllCombinations, getNeighbourPosition, getPossibleNeghbours functions. Modyfikacja funkcji getBoardPositions
    
    * First working version: v1
    
    * Add results printing
    
    * Seceond working version: v2 - add comments and descriptions to the functions
    
    * Add .gitignore"
    
    * Change project structure
    
    Co-authored-by: Tomasz Glowka <tomek3396@gmail.com>

--- ReadFileModule.hs
+++ ReadFileModule.hs
@@ -1,12 +1,15 @@
 module ReadFileModule where
 
 {-
-  Wczytuje łamigłówkę z pliku o podanej nazwie
-  Przykład: readPuzzle "puzzle.txt"
+  Description: The 'readPuzzle' function loads the puzzle from file.
+  Arguments:
+  - String - the name of the file
+  Returns:
+  - IO [String] - the loaded puzzle
 -}
 
 readPuzzle :: String -> IO [String]
 readPuzzle filename = do
-  contents <- readFile filename -- odczytaj całą zawartość pliku
-  let puzzle = read contents :: [String] -- utwórz listę napisów (zob. klasa typów Read)
+  contents <- readFile filename
+  let puzzle = read contents :: [String]
   return puzzle
